69fd3bde0f0c82c4f60f88bf98dc77b1bc2c7287
xueying126 <xueying19981206@gmail.com>
2024-04-03 13:24:11 +0200

    add 6d pose

--- s3_process_init.py
+++ s3_process_init.py
@@ -1,5 +1,6 @@
 '''
 Regulate the motion to start from origin
+Add 6D rotation representation 
 
 Input  folder: './joints', 
 Output folder: './HumanML3D/smpl'
@@ -7,7 +8,8 @@ Output folder: './HumanML3D/smpl'
 import os
 import numpy as np
 from tqdm import tqdm
-import os
+from tools import utils_transform
+import torch
 
 def process(file_name, data_dir, save_dir):
     data = np.load(os.path.join(data_dir, file_name),allow_pickle=True).item() # 'bdata_poses', 'bdata_trans', 'betas', 'gender', 'jtr'
@@ -15,16 +17,26 @@ def process(file_name, data_dir, save_dir):
     trans = data['bdata_trans']
 
     '''Move the first pose to origin'''
-    trans[:, [0, 1]] -= trans[0, [0, 1]] # the global body position x, y 
+    trans[:, [0, 1]] -= trans[0, [0, 1]] # the global position x, y 
 
     # '''All initially face Y+/Z+'''
     # init_root_rot = poses[0, [0, 1, 2]] # the global root orientation
+
+    '''from axis-angle to 6D'''
+    pose_aa = torch.Tensor(poses).reshape(-1,3)
+    pose_6d = utils_transform.aa2sixd(pose_aa).reshape(poses.shape[0],-1).numpy() # [fn, 312]
+
+    # '''from 6D to axis-angle'''
+    # rot_6d =  torch.Tensor(rot_6d).reshape(-1, 6)
+    # rot_aa_b = utils_transform.sixd2aa(rot_6d).reshape(poses.shape[0], -1).numpy()
+
 
     new_data={
         'bdata_poses': poses, 
         'bdata_trans':trans, 
         'betas':data['betas'], 
         'gender':data['gender'],
+        'pose_6d': pose_6d,
     }
     np.save(os.path.join(save_dir, file_name), new_data)
 
